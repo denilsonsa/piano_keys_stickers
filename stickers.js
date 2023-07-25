3377c322ee16c513552b1313bf6c2b83055b1f41
Denilson Sá Maia <denilsonsa@gmail.com>
2023-07-25 11:27:25 +0200

    Major refactoring: no more Note global state
    The reactivity part has been extracted out of the global Note class.
    Note is now immutable and self-contained. It's now possible to have
    different note_opts on the same page.
    
    I'm not totally happy with the duplication and boilerplate on the Vue
    code.

--- stickers.js
+++ stickers.js
@@ -3,9 +3,8 @@ import Note from './note.js';
 
 
 export default {
-	setup() {
-		const black_notes_count = ref(0);
-		const white_notes_count = ref(0);
+	setup(props, context) {
+		const clefs_style = ref('visible');
 		const min_note_code = ref(36);
 		const max_note_code = ref(84);
 		const dimensions = reactive({
@@ -14,37 +13,50 @@ export default {
 			height_white: 30,
 			height_black: 5,
 		});
+		const temp_note = new Note(60);  // Just to fetch the default values for the options.
+		const note_opts = reactive({
+			...temp_note.opts
+		});
+
+		const all_notes_and_stats = computed(() => {
+			let white_notes_count = 0;
+			let black_notes_count = 0;
+			let all_notes = [];
+
+			let note = new Note(min_note_code.value, note_opts);
+			while (note.code <= max_note_code.value) {
+				if (note.is_major) {
+					white_notes_count++;
+				} else {
+					black_notes_count++;
+				}
+				all_notes.push(note);
+				note = note.next_semitone();
+			}
+
+			return {
+				white_notes_count,
+				black_notes_count,
+				all_notes,
+			};
+		});
 
 		return {
-			clefs_style: ref('visible'),
+			clefs_style,
+			min_note_code,
+			max_note_code,
 			dimensions,
+			note_opts,
 			inline_styles: computed(() => ({
 				'--white-width': `${ dimensions.width_white }mm`,
 				'--black-width': `${ dimensions.width_black }mm`,
 				'--white-height': `${ dimensions.height_white }mm`,
 				'--black-height': `${ dimensions.height_black }mm`,
 			})),
-			black_notes_count,
-			white_notes_count,
-			min_note_code,
-			max_note_code,
-			note_conf: Note.reactive,
-			note_iterator: function*() {
-				black_notes_count.value = 0;
-				white_notes_count.value = 0;
-
-				let note = new Note(min_note_code.value);
-				while (note.code <= max_note_code.value) {
-					if (note.is_major) {
-						white_notes_count.value++;
-					} else {
-						black_notes_count.value++;
-					}
-					yield note;
-					note = note.next_semitone();
-					// note = note.next_major();
-				}
-			},
+			black_notes_count: computed(() => all_notes_and_stats.value.black_notes_count),
+			white_notes_count: computed(() => all_notes_and_stats.value.white_notes_count),
+			notes_count: computed(() => all_notes_and_stats.value.all_notes.length),
+			all_notes: computed(() => all_notes_and_stats.value.all_notes),
 			Note: Note,
 		}
 	}
